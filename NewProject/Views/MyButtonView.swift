6fc98ae53dfc8c493331c8c3a220e846490d27a0
Oleh Zimin <ziminoleh@gmail.com>
2024-09-15 18:45:29 +0200

    Add new group Views with ButtonView

---
+++ NewProject/Views/MyButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  MyButtonView.swift
+//  NewProject
+//
+//  Created by O Z on 15.09.2024.
+//
+
+import SwiftUI
+
+struct MyButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyButtonView()
+}
