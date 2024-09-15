048e973574131f0339993d12a9154941379ad6b6
Oleh Zimin <ziminoleh@gmail.com>
2024-09-15 16:55:40 +0200

    Create NewProject

---
+++ NewProject/NewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewProjectApp.swift
+//  NewProject
+//
+//  Created by O Z on 15.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct NewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
